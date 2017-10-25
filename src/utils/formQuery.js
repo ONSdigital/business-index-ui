381dd499f51a2c8b9a87b9163f0e236acad144ba
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-10-25 14:23:08 +0100

    Add encodeSpecialChars method to formQuery
    - For encoding businessName
    - Copied method from business-index-ui

--- src/utils/formQuery.js
+++ src/utils/formQuery.js
@@ -3,6 +3,9 @@ const { SEPERATOR, LIMIT, ES_CONCAT, END_SEPERATOR } = config;
 
 export function formMatchQuery(query) {
   const queryArr = Object.keys(query).map(param => {
+    if (param === 'BusinessName') {
+      return `${param}${SEPERATOR}${encodeSpecialChars(query[param])}`;
+    }
     return `${param}${SEPERATOR}${query[param]}`;
   });
   const formattedQuery = `${queryArr.join(` ${ES_CONCAT} `)}${END_SEPERATOR}${LIMIT}`;
@@ -23,4 +26,13 @@ export function formRangeQuery(query) {
   });
   const formattedQuery = `${queryArr.join(` ${ES_CONCAT} `)}${END_SEPERATOR}${LIMIT}`;
   return formattedQuery;
+}
+
+// Encode special chars in businessName (e.g. £ etc.)
+export function encodeSpecialChars(businessName) {
+  const encodedName = encodeURIComponent(businessName);
+  if (encodedName.includes('%A3')) {
+    encodedName.replace('%A3,%C2%A3');
+  }
+  return encodedName;
 }
