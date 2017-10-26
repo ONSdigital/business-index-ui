9e1f52ac05dbc765638f3248b6556a7d04d96707
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-10-26 08:48:46 +0100

    Add mock API Gateway related code
    - Copy from sbr-ui fix/authentication

---
+++ server/apiGateway.js
@@ -0,0 +1,143 @@
+'use strict';
+
+/* eslint strict: "off" */
+/* eslint no-console: "off" */
+/* eslint comma-dangle: ["error", "never"] */
+
+const express = require('express');
+const morgan = require('morgan');
+const myParser = require('body-parser');
+const compression = require('compression');
+const bcrypt = require('bcryptjs');
+const genSalt = require('./helpers/salt.js');
+const rp = require('request-promise');
+const timeouts = require('./config/timeouts');
+const urls = require('./config/urls');
+const uuidv4 = require('uuid/v4');
+const logger = require('./logger');
+
+const PORT = process.env.PORT || 3002;
+
+// Get the admin/user credentials from environment variables
+const ADMIN_USERNAME = process.env.BI_UI_TEST_ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.BI_UI_TEST_ADMIN_PASSWORD;
+const USER_USERNAME = process.env.BI_UI_TEST_USER_USERNAME;
+const USER_PASSWORD = process.env.BI_UI_TEST_USER_PASSWORD;
+
+// Create the hashed password using the salt
+const ADMIN_HASHED_PASSWORD = bcrypt.hashSync(ADMIN_PASSWORD, genSalt(ADMIN_USERNAME));
+const USER_HASHED_PASSWORD = bcrypt.hashSync(USER_PASSWORD, genSalt(USER_USERNAME));
+
+// We use the users JSON as a mock database holding { username: hashed_password }
+const users = {};
+users[ADMIN_USERNAME] = ADMIN_HASHED_PASSWORD;
+users[USER_USERNAME] = USER_HASHED_PASSWORD;
+
+// We need to store all the valid API keys that uuidv4() has made
+const validApiKeys = {};
+
+const app = express();
+app.use(compression()); // gzip all responses
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+app.use(myParser.json()); // For parsing body of POSTs
+
+app.post('/auth', (req, res) => {
+  // Get the username/password from the body of the POST
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // If the provided username/password match the username/password in the users JSON,
+  // return an API key and the user role
+  if (users[username] && users[username] === password) {
+    logger.info('Creating API Key for user');
+    const key = uuidv4();
+    validApiKeys[key] = username;
+    res.setHeader('Content-Type', 'application/json');
+    return res.send(JSON.stringify({
+      key,
+      role: username
+    }));
+  }
+  return res.sendStatus(401);
+});
+
+app.get('/bi/*', (req, res) => {
+  const url = getUrlEndpoint(req.originalUrl);
+  logger.info(`Rerouting GET API request to ${url}`);
+
+  // Check if the API Key is valid
+  const apiKey = req.get('Authorization');
+  if (validApiKey(apiKey)) {
+    getApiEndpoint(`${urls.API_URL}${url}`)
+    .then((response) => {
+      logger.info('Returning re-routed GET API request');
+      return res.send(response);
+    })
+    .catch((error) => {
+      logger.error('Error rerouting GET request');
+      return res.status(error.statusCode).send(error);
+    });
+  } else {
+    return res.sendStatus(401);
+  }
+});
+
+app.post('/bi/*', (req, res) => {
+  const url = getUrlEndpoint(req.originalUrl);
+  logger.info(`Rerouting POST API request to ${url}`);
+
+  // Check if the API Key is valid
+  const apiKey = req.get('Authorization');
+  if (validApiKey(apiKey)) {
+    const postBody = req.body;
+    postApiEndpoint(`${urls.API_URL}${url}`, postBody)
+      .then((response) => {
+        logger.info('Returning re-routed POST API request');
+        return res.send(response);
+      })
+      .catch((error) => {
+        logger.error('Error rerouting POST request');
+        return res.status(error.statusCode).send(error);
+      });
+  } else {
+    return res.sendStatus(401);
+  }
+});
+
+function validApiKey(apiKey) {
+  return validApiKeys[apiKey];
+}
+
+function getUrlEndpoint(url) {
+  return url.substring(url.indexOf('/', 1), url.length);
+}
+
+function getApiEndpoint(url) {
+  logger.debug(`GET API endpoint for ${url}`);
+  const options = {
+    method: 'GET',
+    uri: url,
+    timeout: timeouts.API_GET
+  };
+
+  return rp(options);
+}
+
+function postApiEndpoint(url, postBody) {
+  logger.debug(`POST API endpoint for ${url}`);
+  const options = {
+    method: 'POST',
+    uri: url,
+    timeout: timeouts.API_POST,
+    headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+    body: JSON.stringify(postBody), // '{"updatedBy":"name","vars":{"ent_name":"name"}}',
+    json: false
+  };
+
+  return rp(options);
+}
+
+app.listen(PORT, () => {
+  console.log(`bi-ui-mock-api-gateway listening on port ${PORT}!`);
+  logger.info(`bi-ui-mock-api-gateway listening on port ${PORT}!`);
+});
--- server/index.js
+++ server/index.js
@@ -1,18 +1,50 @@
 'use strict';
 
-// Rule exceptions:
 /* eslint strict: "off" */
 /* eslint no-console: "off" */
 
+const fork = require('child_process').fork;
 const app = require('./app');
 const logger = require('./logger');
 
 const PORT = process.env.PORT || 3001;
+
+// On a local environment, we mock the API Gateway with the a node script on localhost:3002
+const child = (process.env.ENV === 'local') ? fork('./server/apiGateway') : null;
 
 logger.level = 'debug';
 logger.info('Started Winston logger & created log file');
 
 app.maxSockets = 500;
 app.listen(PORT, () => {
+  console.log(`bi-ui-node-server listening on port ${PORT}!`);
   logger.info(`bi-ui-node-server listening on port ${PORT}!`);
 });
+
+// Cleanup Code - for before the application exits
+process.stdin.resume(); // so the program will not close instantly
+
+function exitHandler(options, err) {
+  if (options.cleanup) {
+    if (process.env.ENV === 'local') {
+      console.log('Killing child process (bi-ui-mock-api-gateway)...');
+      logger.info('Killing child process (bi-ui-mock-api-gateway)...');
+      child.kill('SIGINT');
+    }
+  }
+  if (err) console.log(err.stack);
+  if (options.exit) process.exit();
+}
+
+// Do something when app is closing
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+
+// Catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+
+// Catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+
+// Catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
