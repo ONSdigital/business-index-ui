b402c1ab364b9f422c9816b77f8c9593a529570a
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-12-05 14:55:25 +0000

    Use env vars for URLS in the frontend

--- src/config/api-urls.js
+++ src/config/api-urls.js
@@ -1,6 +1,6 @@
 const apiUrls = {
-  AUTH_URL: 'http://localhost:3001',
-  REROUTE_URL: 'http://localhost:3001/api',
+  AUTH_URL: process.env.REACT_APP_AUTH_URL,
+  REROUTE_URL: process.env.REACT_APP_API_URL,
   SEARCH_ENDPOINT: 'search/',
   BUSINESS_ENDPOINT: 'business',
   API_VERSION: 'v1',
