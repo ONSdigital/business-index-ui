de21f4958038d3c38b00b28cb367b0df038e6c2d
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-12-15 11:52:43 +0000

    Remove unused helperMethods

--- src/utils/helperMethods.js
+++ src/utils/helperMethods.js
@@ -1,47 +1,5 @@
 // @flow
 
-export function countStatus(history: Array<{}>, status: number) {
-  if (history === []) {
-    return 0;
-  }
-  return history.filter(h => h.HTTPCode === status).length;
-}
-
-export function countStatusBetween(history: Array<{}>, status: object) {
-  if (history === []) {
-    return 0;
-  }
-  return history.filter(h => h.HTTPCode >= status.min && h.HTTPCode <= status.max).length;
-}
-
-export function formatResultsTable(results: Array<{}>) {
-  const formattedResults: Array<{}> = [];
-  results.forEach((i) => {
-    const record: Object = i;
-    if (record.source === 'VAT' || record.source === 'Legal Unit') {
-      record.name = record.businessName;
-    }
-    formattedResults.push(record);
-  });
-  return formattedResults;
-}
-
-export function getValueByKey(object: {}, toGet: string) {
-  return (toGet in object) ? object[toGet] : '';
-}
-
-export function getChildValues(json: {}, compareString: string) {
-  const arr: Array<{}> = [];
-  Object.keys(json).forEach((k) => {
-    if (json[k] === compareString) {
-      const obj: {} = {};
-      obj[compareString] = k;
-      arr.push(obj);
-    }
-  });
-  return arr;
-}
-
 export function maxSize(...args) {
   return args.reduce((a, b) => (b.length > a ? b.length : a), 0);
 }
