dd1cd293964a594f0a52a4cecfc8d52e03c7e638
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-01-18 09:03:13 +0000

    Add documentation to methods in /utils/export.js

--- src/utils/export.js
+++ src/utils/export.js
@@ -2,6 +2,14 @@ import config from '../config/export';
 
 const { FILE_NAME } = config;
 
+/**
+ * @function exportCSV
+ *
+ * @param  {string} header - The header to use in the CSV
+ * @param  {Array<JSON>} results - The results to save in a CSV file
+ *
+ * @return {string} A string of all the results in CSV format
+ */
 export function exportCSV(header, results) {
   const cols = ['id', 'businessName', 'postCode', 'industryCode', 'legalStatus', 'tradingStatus', 'turnover', 'employmentBands', 'companyNo'];
   const rows = results.map(
@@ -12,41 +20,28 @@ export function exportCSV(header, results) {
   return `${header}\r\n`.concat(rows.join(''));
 }
 
-export function exportCSVOld(header, results) {
-  const columnNames = ['id', 'businessName', 'postCode', 'industryCode', 'legalStatus', 'tradingStatus', 'turnover', 'employmentBands', 'companyNo'];
-  let CSV = '';
-  CSV += `${header}\r\n`; // Firstly, insert the header
-  // Go through the results, putting them in the CSV string
-  for (let i = 0; i < results.length; i += 1) {
-    let row = '';
-    // If there is missing data, insert an empty string
-    for (let j = 0; j < columnNames.length; j += 1) {
-      if (results[i][columnNames[j]] === undefined) {
-        row += '"",';
-      } else {
-        row += `"${results[i][columnNames[j]]}",`;
-      }
-    }
-    CSV += `${row}\r\n`;
-  }
-  return CSV;
-}
-
+/**
+ * @function downloadCSV
+ *
+ * @param  {Array<JSON>} results - The results to save in a CSV file
+ *
+ */
 export function downloadCSV(results) {
   const header = 'UBRN,Business Name,PostCode,Industry Code,Legal Status,Trading Status,Turnover,Employment,Company Reference Number';
-  const csv = exportCSV(header, results, FILE_NAME);
+  const csv = exportCSV(header, results);
   const uri = `data:text/csv;charset=utf-8,${escape(csv)}`;
   const link = document.createElement('a');
   link.href = uri;
-  const filename = `${FILE_NAME}.csv`;
-  link.download = filename;
-  // Below append/remove child is to ensure the download button works on
-  // Firefox, link.click()
-  document.body.appendChild(link);
+  link.download = `${FILE_NAME}.csv`;
   link.click();
-  document.body.removeChild(link);
 }
 
+/**
+ * @function downloadJSON
+ *
+ * @param  {Array<JSON>} results - The results to save in a JSON file
+ *
+ */
 export function downloadJSON(results) {
   const jsonStr = JSON.stringify(results, null, 2);
   const uri = `data:text/json;charset=utf-8,${escape(jsonStr)}`;
