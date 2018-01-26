f57e1b6d9d8778c5cc1117f3dd76df430b6e6368
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-01-26 12:36:26 +0000

    Add env var to apiGateway config
    - Use passed in env var for server url, so that we can pass in a docker url

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,7 +1,7 @@
 const urls = {
   AUTH_URL: process.env.SERVER_AUTH_URL,
   API_GW: process.env.SERVER_API_GW_URL,
-  API_URL: 'http://localhost:9000',
+  API_URL: process.env.API_URL,
   API_VERSION: 'v1',
 };
 
