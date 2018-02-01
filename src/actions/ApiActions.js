cf1e85d5c1524fb9ece221ed05c0bb0cbde95295
ONS-Tom <Tom.Cooling@ons.gov.uk>
2018-02-01 11:51:17 +0000

    Fix/api request refactor (#30)
    * Remove unused onChange method from auth.js
    
    * Add refactored fetch usage for searching
    
    - Add accessAPI which is basically a helper to preset some commonly used config for fetch
    
    * Add refactored InfoActions with new fetch
    
    - Use accessAPI method in InfoActions
    
    * Refactor login action to use accessAPI
    
    * Add refactored logout LoginAction
    
    * Add refactored logout action in LoginActions
    
    * Remove unused /utils files
    
    - Use default export in accessAPI.js
    
    * Add more informative api request error messages
    
    * Fix wrong last update usage in Info page
    
    * Fix Jenkinsfile library branch
    
    - Use master, not a deleted branch

--- src/actions/ApiActions.js
+++ src/actions/ApiActions.js
@@ -1,5 +1,8 @@
 import { SET_RESULTS, SET_FORMATTED_QUERY, SET_SEARCH_ERROR_MESSAGE, SENDING_SEARCH_REQUEST, SET_QUERY, SET_HEADERS } from '../constants/ApiConstants';
-import apiSearch from '../utils/apiSearch';
+import accessAPI from '../utils/accessAPI';
+import config from '../config/api-urls';
+
+const { REROUTE_URL, API_VERSION } = config;
 
 // The search action creator can be used for Match/Range/UBRN searches
 export function search(query, formQuery, jsonKey) {
@@ -10,18 +13,22 @@ export function search(query, formQuery, jsonKey) {
     dispatch(setQuery(SET_QUERY, query, jsonKey));
     const formattedQuery = formQuery(query);
     dispatch(setFormattedQuery(SET_FORMATTED_QUERY, formattedQuery, jsonKey));
-    apiSearch.search(formattedQuery, (success, data) => {
+
+    accessAPI(REROUTE_URL, 'POST', sessionStorage.accessToken, JSON.stringify({
+      method: 'GET',
+      endpoint: `${API_VERSION}/${formattedQuery}`,
+    }), (success, data) => {
       dispatch(sendingRequest(SENDING_SEARCH_REQUEST, false, jsonKey));
       if (success) {
         // This is a workaround for the API returning 200 {} for no results, should be 404
-        if (Object.keys(data.results).length === 0 && data.results.constructor === Object) {
+        if (Object.keys(data.json).length === 0 && data.json.constructor === Object) {
           dispatch(setErrorMessage(SET_SEARCH_ERROR_MESSAGE, '404: No results found.', jsonKey));
         } else {
           if (jsonKey === 'ubrn') {
             // Wrap the results in an array as we only get {} from the API
-            dispatch(setResults(SET_RESULTS, [data.results], jsonKey));
+            dispatch(setResults(SET_RESULTS, [data.json], jsonKey));
           } else {
-            dispatch(setResults(SET_RESULTS, data.results, jsonKey));
+            dispatch(setResults(SET_RESULTS, data.json, jsonKey));
           }
           dispatch(setHeaders(SET_HEADERS, data.response, jsonKey));
         }
---
+++ src/utils/accessAPI.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @const accessAPI - This encapsulates common fetch parameters
+ *
+ * @param {String} url - URL to fetch from
+ * @param {String} method - The HTTP method
+ * @param {String} auth - Either basic authentication or the accessToken
+ * @param {String} body - The request body (as a JSON string)
+ * @param {Function} callback - The method to call with the fetch results
+ *
+ * @return {Function}
+ *
+ */
+const accessAPI = (url, method, auth, body, callback) => {
+  return fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: auth,
+    },
+    body,
+  }).then((response) => {
+    switch (response.status) {
+      case 200: return response.json().then((json) => callback(true, { json }));
+      case 401: return callback(false, { message: 'Authentication problem. Please ensure you are logged in.' });
+      case 500: return callback(false, { message: 'Server error. Please contact your system administrator.' });
+      default: return callback(false, { message: `${response.status} error.` });
+    }
+  }).catch((err) => callback(false, { message: `Server error: request timed out. ${err}` }));
+};
+
+export default accessAPI;
