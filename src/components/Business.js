dc2fbef4e27c439f10927d39afa74d9917462a26
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-03-20 15:52:03 +0000

    Fix issue with <table> child, use <tbody>
    - https://stackoverflow.com/questions/41716528/react-tr-cannot-appear-as-a-child-of-td-see-comment-td-tr

--- src/components/Business.js
+++ src/components/Business.js
@@ -16,23 +16,27 @@ const Business = ({ business, toHighlight }) => {
     <div className="search-item-container">
       <h3 className="saturn sml-margin">{(toHighlight !== '') ? getHighlightedText(business, toHighlight) : business.businessName}</h3>
       <table className="mars">
-        <tr><th className="table-grey-text">UBRN</th><td>{business.id}</td></tr>
-        <tr><th className="table-grey-text">Address</th><td>123 Mini Street, Exeter, EX13 8DD</td></tr>
-        <tr><th className="table-grey-text">Industry</th><td>{business.industryCode} – {description}</td></tr>
-        <tr><th className="table-grey-text">Trading status</th><td>{tradingStatusBands[business.tradingStatus]}</td></tr>
-        <tr><th className="table-grey-text">Legal status</th><td>{legalStatusBands[business.legalStatus]}</td></tr>
-        <tr><th className="table-grey-text">Employment band</th><td>{employmentBands[business.employmentBands]}</td></tr>
-        <tr><th className="table-grey-text">Turnover band</th><td>{turnoverBands[business.turnover]}</td></tr>
+        <tbody>
+          <tr><th className="table-grey-text">UBRN</th><td>{business.id}</td></tr>
+          <tr><th className="table-grey-text">Address</th><td>123 Mini Street, Exeter, EX13 8DD</td></tr>
+          <tr><th className="table-grey-text">Industry</th><td>{business.industryCode} – {description}</td></tr>
+          <tr><th className="table-grey-text">Trading status</th><td>{tradingStatusBands[business.tradingStatus]}</td></tr>
+          <tr><th className="table-grey-text">Legal status</th><td>{legalStatusBands[business.legalStatus]}</td></tr>
+          <tr><th className="table-grey-text">Employment band</th><td>{employmentBands[business.employmentBands]}</td></tr>
+          <tr><th className="table-grey-text">Turnover band</th><td>{turnoverBands[business.turnover]}</td></tr>
+        </tbody>
       </table>
       <div className="guidance js-details" data-show-label="Show reference numbers" data-hide-label="Hide reference numbers">
         <a className="guidance__link js-details-trigger js-details-label mars" aria-controls="guidance-answer" aria-expanded="false" id="guidance-trigger" data-guidance-trigger="true" href="#guidance-answer">Show reference numbers</a>
         <div className="guidance__main js-details-body" id="guidance-answer-body" aria-hidden="true">
           <div className="guidance__content new">
             <table>
-              <tr>
-                <th className="table-grey-text-reveal">VAT</th>
-                <td>32098007900</td>
-              </tr>
+              <tbody>
+                <tr>
+                  <th className="table-grey-text-reveal">VAT</th>
+                  <td>32098007900</td>
+                </tr>
+              </tbody>
             </table>
           </div>
         </div>
