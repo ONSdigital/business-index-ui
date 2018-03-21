6982f1db7713e961df746afd6269acc7a4114344
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-03-21 12:53:13 +0000

    Add type=text to SearchForm text inputs

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -16,10 +16,10 @@ import { employmentBands, legalStatusBands, turnoverBands, tradingStatusBands } 
 class SearchForm extends React.Component {
   render = () => (
     <form className={`form ${this.props.className}`}>
-      <TextInput id="BusinessName" size={this.props.size} onChange={this.props.onChange} autoFocus={this.props.autoFocus} type="search" label="Business name" value={this.props.initialValues.BusinessName} ref={ip => (this.childTextInput = ip)} />
-      <TextInput id="Id" size={this.props.size} onChange={this.props.onChange} type="search" label="UBRN" value={this.props.initialValues.Id} />
-      <TextInput id="Ref" size={this.props.size} onChange={this.props.onChange} type="search" label="CRN, VAT, PAYE" value={this.props.initialValues.Ref} />
-      <TextInput id="PostCode" size={this.props.size} onChange={this.props.onChange} type="postcode" label="Postcode" value={this.props.initialValues.PostCode} />
+      <TextInput id="BusinessName" size={this.props.size} onChange={this.props.onChange} autoFocus={this.props.autoFocus} type="text" label="Business name" value={this.props.initialValues.BusinessName} ref={ip => (this.childTextInput = ip)} />
+      <TextInput id="Id" size={this.props.size} onChange={this.props.onChange} type="text" label="UBRN" value={this.props.initialValues.Id} />
+      <TextInput id="Ref" size={this.props.size} onChange={this.props.onChange} type="text" label="CRN, VAT, PAYE" value={this.props.initialValues.Ref} />
+      <TextInput id="PostCode" size={this.props.size} onChange={this.props.onChange} type="text" label="Postcode" value={this.props.initialValues.PostCode} />
       <TextInputRange id="IndustryCode" size={this.props.size} onChange={this.props.onChange} label="Industry classification (SIC)" toggleText="Search range" value={this.props.initialValues.IndustryCode} />
       <SelectMultipleInput id="EmploymentBands" size={this.props.size} value={this.props.initialValues.EmploymentBands} onChange={this.props.onChange} label="Employment Bands" bands={employmentBands} />
       <SelectMultipleInput id="Turnover" size={this.props.size} value={this.props.initialValues.Turnover} onChange={this.props.onChange} label="Turnover Bands" bands={turnoverBands} />
