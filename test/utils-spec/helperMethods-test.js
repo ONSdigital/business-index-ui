24b8bba88c756f33711b55c81f9326e3624ecaae
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-03-22 09:14:40 +0000

    Add unit tests for other helperMethods code

--- test/utils-spec/helperMethods-test.js
+++ test/utils-spec/helperMethods-test.js
@@ -1,4 +1,7 @@
-import { maxSize, formatData } from '../../src/utils/helperMethods';
+import {
+  maxSize, formatData, everyKeyMatches, handleFormChange,
+  formSelectJson, anyKeyEmpty
+} from '../../src/utils/helperMethods';
 
 describe("maxSize", () => {
   it("gets the maxSize of given arrays", () => {
@@ -76,5 +79,93 @@ describe("formatData", () => {
     }];
     const formatted = formatData(business);
     expect(JSON.stringify(correctFormat)).toBe(JSON.stringify(formatted));
+  });
+});
+
+describe("everyKeyMatches", () => {
+  it("returns true if every key matches a value", () => {
+    const result = everyKeyMatches({ a: '', b: '', c: '' }, '');
+    expect(result).toBe(true);
+  });
+
+  it("returns false if any key matches does not match the value", () => {
+    const result = everyKeyMatches({ a: 'abc', b: '', c: '' }, '');
+    expect(result).toBe(false);
+  });
+
+  it("returns false if any key matches does not match the value (equality test)", () => {
+    const result = everyKeyMatches({ a: '1', b: '1', c: '1' }, 1);
+    expect(result).toBe(false);
+  });
+});
+
+describe("handleFormChange", () => {
+  it("removes empty keys (string)", () => {
+    const result = handleFormChange({ BusinessName: '', PostCode: 'ABC 123' }, 'BusinessName', '');
+    const expected = {
+      PostCode: 'ABC 123',
+    };
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
+
+  it("removes empty keys (object)", () => {
+    const result = handleFormChange({
+      IndustryCode: {
+        min: '',
+        max: '',
+        single: '',
+    }, PostCode: 'ABC 123' }, 'IndustryCode', { min: '', max: '', single: '' });
+    const expected = {
+      PostCode: 'ABC 123',
+    };
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
+
+  it("removes empty keys (array)", () => {
+    const result = handleFormChange({ PayeRefs: [], PostCode: 'ABC 123' }, 'PayeRefs', []);
+    const expected = {
+      PostCode: 'ABC 123',
+    };
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
+
+  it("transforms businessName to upper case", () => {
+    const result = handleFormChange({ BusinessName: 'a' }, 'BusinessName', 'a');
+    const expected = {
+      BusinessName: 'A',
+    };
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
+
+  it("transforms businessName to upper case", () => {
+    const result = handleFormChange({ PostCode: 'a' }, 'PostCode', 'a');
+    const expected = {
+      PostCode: 'A',
+    };
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
+});
+
+describe("formSelectJson", () => {
+  it("returns true if every key matches a value", () => {
+    const result = formSelectJson({ a: '1', b: '2', c: '3' });
+    const expected = [
+      { label: 'a [1]', value: 'a' },
+      { label: 'b [2]', value: 'b' },
+      { label: 'c [3]', value: 'c' },
+    ];
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
+});
+
+describe("anyKeyEmpty", () => {
+  it("returns true if any key is empty", () => {
+    const result = anyKeyEmpty({ a: '1', b: '2', c: '' });
+    expect(result).toBe(true);
+  });
+
+  it("returns false if no keys are empty", () => {
+    const result = anyKeyEmpty({ a: '1', b: '2', c: '3' });
+    expect(result).toBe(false);
   });
 });
