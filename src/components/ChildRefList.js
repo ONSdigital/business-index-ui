26057e78d2d41c640f37140764641fbae6bd74ed
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-03-28 08:29:48 +0100

    Add fix for ChildRefList, use props not this.state

--- src/components/ChildRefList.js
+++ src/components/ChildRefList.js
@@ -2,10 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LinkButton from '../patterns/LinkButton';
 import Panel from '../patterns/Panel';
-import config from '../config/api-urls';
-import accessAPI from '../utils/accessAPI';
-
-const { REROUTE_URL, API_VERSION, BUSINESS_ENDPOINT } = config;
 
 /**
  * @const ChildRefTable - This is a sub list of the child references of
@@ -16,8 +12,7 @@ const { REROUTE_URL, API_VERSION, BUSINESS_ENDPOINT } = config;
 const ChildRefList = (props) => (
   <div style={{ padding: '20px' }}>
     <LinkButton id="expandRefs" className="mars" text={(props.isLoading) ? 'Loading...' : 'Show reference numbers'} onClick={props.fetchData} loading={false} />
-    <Errorodal show={props.error} message={props.errorMessage} close={props.closeModal} />
-    <Panel id="refsErrorPanel" text={this.state.errorMessage} level="error" show={this.state.error} close={null} marginBottom="1rem" />
+    <Panel id="refsErrorPanel" text={props.errorMessage} level="error" show={props.error} close={null} marginBottom="1rem" />
     {props.finishedLoading &&
       <table>
         <tbody>
